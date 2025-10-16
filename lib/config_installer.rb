b9e176850fe2fbd9146d87ec9785b749b137073c
Luiz Felipe de Castro Vilas Boas <luizfelipecastrovb@gmail.com>
2025-10-15 23:14:40 -0300

    feat: add waybar config (also add to link all config files in the folder)

--- lib/config_installer.rb
+++ lib/config_installer.rb
@@ -13,11 +13,21 @@ class ConfigInstaller
     @to = to
   end
 
-  def add_symlink
+  def add_symlink # rubocop:disable Metrics/MethodLength
     raise IOError, "config file '#{@from}' does not exists" unless File.exist?(@from)
 
     logger.info "Adding system link from #{@from} to #{@to}"
-    FileUtils.mkdir_p(File.dirname(@to))
-    FileUtils.ln_s @from, @to, force: true
+
+    if File.directory? @from
+      pattern = File.join(@from, '*')
+
+      Dir.glob(pattern).each do |filepath|
+        FileUtils.mkdir_p(File.dirname(@to))
+        FileUtils.ln_s(filepath, @to, force: true)
+      end
+    else
+      FileUtils.mkdir_p(File.dirname(@to))
+      FileUtils.ln_s(@from, @to, force: true)
+    end
   end
 end
